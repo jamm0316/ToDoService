84b313e55fca6247fed6d53d2c73e04159de70fb
jamm0316 <jamm0316@naver.com>
2025-08-14 11:50:54 +0900

    refactor: 테스트 코드 import 수정
    1. 불필요한 import문 제거
    
    Resolves #7

--- src/test/java/com/todoservice/greencatsoftware/domain/project/ProjectJpaTest.java
+++ src/test/java/com/todoservice/greencatsoftware/domain/project/ProjectJpaTest.java
@@ -6,16 +6,15 @@ import com.todoservice.greencatsoftware.domain.color.Color;
 import com.todoservice.greencatsoftware.domain.color.ColorRepository;
 import com.todoservice.greencatsoftware.domain.project.entity.Project;
 import jakarta.validation.ConstraintViolationException;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.ActiveProfiles;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DataJpaTest
 @ActiveProfiles("test")
