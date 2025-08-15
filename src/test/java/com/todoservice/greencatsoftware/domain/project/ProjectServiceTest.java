7aa1691fb20f23212f9b323c8645ce2511c601e7
jamm0316 <jamm0316@naver.com>
2025-08-15 22:14:16 +0900

    test: 프로젝트 반환 테스트 추가
    1. 프로젝트 존재 시 반환하는 테스트 케이스 추가한다.
    2. `assertThatThrownBy` 및 새로운 import 문 추가한다.
    3. 테스트 가독성 및 안정성을 향상한다.
    
    Resolves #14

--- src/test/java/com/todoservice/greencatsoftware/domain/project/ProjectServiceTest.java
+++ src/test/java/com/todoservice/greencatsoftware/domain/project/ProjectServiceTest.java
@@ -1,5 +1,7 @@
 package com.todoservice.greencatsoftware.domain.project;
 
+import com.todoservice.greencatsoftware.common.baseResponse.BaseResponseStatus;
+import com.todoservice.greencatsoftware.common.exception.BaseException;
 import com.todoservice.greencatsoftware.domain.project.entity.Project;
 import com.todoservice.greencatsoftware.domain.project.model.ProjectRepository;
 import com.todoservice.greencatsoftware.domain.project.model.ProjectService;
@@ -14,6 +16,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -45,5 +48,19 @@ public class ProjectServiceTest {
         assertThat(projects).hasSize(2);
         assertThat(projects.get(0).getName()).isEqualTo("projectA");
         assertThat(projects.get(1).getName()).isEqualTo("projectB");
+    }
+
+    @Test
+    @DisplayName("프로젝트가 존재하면 반환한다.")
+    public void 프로젝트_존재시_반환() throws Exception {
+        //given
+        Project project = new Project();
+        project.setId(1L);
+        project.setName("projectA");
+        when(projectRepository.findById(project.getId())).thenReturn(java.util.Optional.of(project));
+
+        //then
+        assertThat(projectService.getProjectByIdOrThrow(1L)).isEqualTo(project);
+        assertThat(projectService.getProjectByIdOrThrow(1L).getName()).isEqualTo("projectA");
     }
 }
