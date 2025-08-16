c6a9ddb702c2acfadd5c9b10a5a0d74bc4b56e08
jamm0316 <jamm0316@naver.com>
2025-08-17 04:45:25 +0900

    feat: 태스크 생성 기능 추가 및 모델 매퍼 의존성 주입
    1. 태스크 생성 메서드(createTask) 추가한다.
    2. DTO -> 엔티티 매핑 메서드(toEntity) 추가한다.
    3. ModelMapper 의존성 추가 및 주입 설정한다.
    
    Resolves #19

--- src/main/java/com/todoservice/greencatsoftware/domain/task/model/TaskService.java
+++ src/main/java/com/todoservice/greencatsoftware/domain/task/model/TaskService.java
@@ -2,8 +2,10 @@ package com.todoservice.greencatsoftware.domain.task.model;
 
 import com.todoservice.greencatsoftware.common.baseResponse.BaseResponseStatus;
 import com.todoservice.greencatsoftware.common.exception.BaseException;
+import com.todoservice.greencatsoftware.domain.task.dto.TaskCreateRequest;
 import com.todoservice.greencatsoftware.domain.task.entity.Task;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,6 +14,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class TaskService {
     private final TaskRepository taskRepository;
+    private final ModelMapper modelMapper;
 
     public List<Task> listTask() {
         return taskRepository.findAll();
@@ -20,5 +23,13 @@ public class TaskService {
     public Task getTaskByIdOrThrow(Long id) {
         return taskRepository.findById(id)
                 .orElseThrow(() -> new BaseException(BaseResponseStatus.NOT_FOUND_TASK));
+    }
+
+    public Task createTask(TaskCreateRequest newTaskDTO) {
+        return taskRepository.save(toEntity(newTaskDTO));
+    }
+
+    private Task toEntity(TaskCreateRequest newTaskDTO) {
+        return modelMapper.map(newTaskDTO, Task.class);
     }
 }
