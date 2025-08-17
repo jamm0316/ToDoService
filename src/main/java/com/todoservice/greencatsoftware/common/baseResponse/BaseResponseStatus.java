38fd5b00204ea1e1b14d670ffb89bde67ea41cc6
jamm0316 <jamm0316@naver.com>
2025-08-18 04:14:39 +0900

    feat: 추가된 일정 관련 오류 상태 상수 정의
    1. 시작 날짜 없는 시간 사용 오류 상태 추가한다.
    2. 상태 코드 8002 추가 및 메시지 정의한다.
    
    Resolves #22

--- src/main/java/com/todoservice/greencatsoftware/common/baseResponse/BaseResponseStatus.java
+++ src/main/java/com/todoservice/greencatsoftware/common/baseResponse/BaseResponseStatus.java
@@ -52,6 +52,7 @@ public enum BaseResponseStatus {
      * 8000: Schedule 오류
      */
     INVALID_DATE_ORDER(false, 8001, "마감일은 시작일보다 이후여야 합니다"),
+    MISSING_START_DATE_FOR_TIME(false, 8002, "시작 시간을 사용하려면 시작 날짜가 필요합니다"),
     ;
 
 
