f3a1dfe369e46af2873cd895264fab86037fac82
jamm0316 <jamm0316@naver.com>
2025-08-18 04:21:10 +0900

    feat: BaseResponseStatus에 마감 시간 값 누락 상태 추가
    1. MISSING_DUE_TIME_VALUE 상태 추가하여 마감 시간 값이 없을 시 처리한다.
    2. 상태 코드 8005 및 관련 메시지 설정한다.
    
    Resolves #22

--- src/main/java/com/todoservice/greencatsoftware/common/baseResponse/BaseResponseStatus.java
+++ src/main/java/com/todoservice/greencatsoftware/common/baseResponse/BaseResponseStatus.java
@@ -55,6 +55,7 @@ public enum BaseResponseStatus {
     MISSING_START_DATE_FOR_TIME(false, 8002, "시작 시간을 사용하려면 시작 날짜가 필요합니다"),
     MISSING_DUE_DATE_FOR_TIME(false, 8003, "마감 시간을 사용하려면 마감 날짜가 필요합니다"),
     MISSING_START_TIME_VALUE(false, 8004, "시작 시간이 활성화되면 시작 시간 값이 필요합니다"),
+    MISSING_DUE_TIME_VALUE(false, 8005, "마감 시간이 활성화되면 마감 시간 값이 필요합니다"),
     ;
 
 
