35295b3f076c93ec0fdbb215e14bc73182886148
jamm0316 <jamm0316@naver.com>
2025-08-18 18:14:44 +0900

    refactor: TaskRepository 클래스 삭제
    1. 불필요한 TaskRepository 클래스를 삭제한다.
    2. 관련된 도메인 코드 정리를 통해 코드 구조를 개선한다.
    
    Resolves #22

--- src/main/java/com/todoservice/greencatsoftware/domain/task/model/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package com.todoservice.greencatsoftware.domain.task.model;
-
-import com.todoservice.greencatsoftware.domain.task.entity.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}
