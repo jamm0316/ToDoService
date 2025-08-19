de9bcddf88f2f8c1eed3136a05e0f7a8e385ac97
jamm0316 <jamm0316@naver.com>
2025-08-19 16:43:50 +0900

    test: ProjectServiceTest 테스트 추가
    1. getProjectByIdOrThrow 테스트 케이스 추가한다.
    2. 프로젝트 ID 기반 존재/미존재 사례 테스트 포함한다.
    3. BaseException 발생 확인 로직 포함한다.
    
    Resolves #36

--- src/test/java/com/todoservice/greencatsoftware/domain/project/ProjectServiceTest.java
+++ src/test/java/com/todoservice/greencatsoftware/domain/project/ProjectServiceTest.java
@@ -1,7 +1,9 @@
 package com.todoservice.greencatsoftware.domain.project;
 
+import com.todoservice.greencatsoftware.common.baseResponse.BaseResponseStatus;
 import com.todoservice.greencatsoftware.common.enums.Status;
 import com.todoservice.greencatsoftware.common.enums.Visibility;
+import com.todoservice.greencatsoftware.common.exception.BaseException;
 import com.todoservice.greencatsoftware.domain.color.application.ColorService;
 import com.todoservice.greencatsoftware.domain.color.entity.Color;
 import com.todoservice.greencatsoftware.domain.project.application.ProjectService;
@@ -16,6 +18,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -52,6 +55,26 @@ public class ProjectServiceTest {
         //then
         assertThat(projectService.listProject()).hasSize(2);
         verify(projectRepository).findAll();
+    }
+    
+    @Test
+    @DisplayName("getProjectByIdThrow: 존재, 반환 / 미존재, BaseException 반환")
+    public void getByIdOrThrow() throws Exception {
+        //given
+        LocalDate startDate = LocalDate.of(2025, 1, 1);
+        LocalDate endDate = LocalDate.of(2025, 12, 31);
+        LocalDate actualEndDate = LocalDate.of(2025, 12, 31);
+        Project project = Project.createWithPeriod(Color.create("RED", "#FF000000"), "프로젝트 A", Status.SCHEDULE,
+                startDate, endDate, actualEndDate, "프로젝트 A입니다", true, Visibility.PRIVATE);
 
+        //when
+        when(projectRepository.findById(1L)).thenReturn(Optional.of(project));
+        when(projectRepository.findById(9L)).thenReturn(java.util.Optional.empty());
+
+        //then
+        assertThat(projectService.getProjectByIdOrThrow(1L)).isEqualTo(project);
+        assertThatThrownBy(() -> projectService.getProjectByIdOrThrow(9L))
+                .isInstanceOf(BaseException.class)
+                .hasMessage(BaseResponseStatus.NOT_FOUND_PROJECT.getMessage());
     }
 }
