f4363a1fbb3c4847d23ec001b272341c363e7b36
jamm0316 <jamm0316@naver.com>
2025-08-19 09:19:29 +0900

    feat: Color 엔티티 생성자 및 유효성 검증 로직 추가
    1. 기본 생성자 접근 제한자를 PROTECTED로 설정한다.
    2. 사용자 정의 생성자 추가 및 도메인 유효성 검증 로직 구현한다.
    3. 정적 팩토리 메서드(create) 생성한다.
    4. 이름 및 색상 코드 변경 메서드 추가 및 유효성 검증 로직 구현한다.
    
    Resolves #34

--- src/main/java/com/todoservice/greencatsoftware/domain/color/entity/Color.java
+++ src/main/java/com/todoservice/greencatsoftware/domain/color/entity/Color.java
@@ -1,14 +1,17 @@
 package com.todoservice.greencatsoftware.domain.color.entity;
 
+import com.todoservice.greencatsoftware.common.baseResponse.BaseResponseStatus;
+import com.todoservice.greencatsoftware.common.exception.BaseException;
 import com.todoservice.greencatsoftware.common.superEntity.SuperEntity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
-@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Color extends SuperEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,4 +24,38 @@ public class Color extends SuperEntity {
     @Column(unique = true)
     @NotNull(message = "색상코드는 필수입니다.")
     private String hexCode;
+
+    public Color(String name, String hexCode) {
+        validateDomainInvariants(name, hexCode);
+        this.name = name.trim();
+        this.hexCode = hexCode.trim();
+    }
+
+    private void validateDomainInvariants(String name, String hexCode) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new BaseException(BaseResponseStatus.MISSING_NAME_FOR_COLOR);
+        }
+
+        if (hexCode == null || hexCode.trim().isEmpty()) {
+            throw new BaseException(BaseResponseStatus.MISSING_HEX_CODE_FOR_COLOR);
+        }
+    }
+
+    public static Color create(String name, String hexCode) {
+        return new Color(name, hexCode);
+    }
+
+    public void changeName(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new BaseException(BaseResponseStatus.MISSING_NAME_FOR_COLOR);
+        }
+        this.name = name.trim();
+    }
+
+    public void changeHexCode(String hexCode) {
+        if (hexCode == null || hexCode.trim().isEmpty()) {
+            throw new BaseException(BaseResponseStatus.MISSING_HEX_CODE_FOR_COLOR);
+        }
+        this.hexCode = hexCode.trim();
+    }
 }
