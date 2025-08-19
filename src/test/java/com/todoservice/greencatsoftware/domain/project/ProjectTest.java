da00d8dcbed53b93bc6d210b352fc9478236c00f
jamm0316 <jamm0316@naver.com>
2025-08-19 14:30:37 +0900

    test: 프로젝트 테스트에 기간 포함 생성 테스트 추가
    1. Period VO 사용 검증 위한 테스트 코드 추가한다.
    2. Project 생성 시 기간 정보 포함된 케이스 테스트 작성한다.
    
    Resolves #36

--- src/test/java/com/todoservice/greencatsoftware/domain/project/ProjectTest.java
+++ src/test/java/com/todoservice/greencatsoftware/domain/project/ProjectTest.java
@@ -4,8 +4,11 @@ import com.todoservice.greencatsoftware.common.enums.Status;
 import com.todoservice.greencatsoftware.common.enums.Visibility;
 import com.todoservice.greencatsoftware.domain.color.entity.Color;
 import com.todoservice.greencatsoftware.domain.project.domain.entity.Project;
+import com.todoservice.greencatsoftware.domain.project.domain.vo.Period;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -28,6 +31,40 @@ public class ProjectTest {
         //then
         assertThat(project.getName()).isEqualTo("나의 프로젝트");
         assertThat(project.getStatus()).isEqualTo(Status.SCHEDULE);
+        assertThat(project.getDescription()).isEqualTo("잘해보자구~");
+        assertThat(project.getIsPublic()).isTrue();
+        assertThat(project.getVisibility()).isEqualTo(Visibility.PUBLIC);
+        assertThat(project.getColor().getHexCode()).isEqualTo("FF0000");
+        assertThat(project.getColor().getName()).isEqualTo("RED");
+    }
+    
+    @Test
+    @DisplayName("저장 생성: 기간 포함 생성")
+    public void create_with_period_ok() throws Exception {
+        //given
+        LocalDate startDate = LocalDate.of(2025, 1, 1);
+        LocalDate endDate = LocalDate.of(2025, 12, 31);
+        Color color = Color.create("RED", "FF0000");
+        Period period = Period.of(startDate, endDate, null);
+
+        //when
+        Project project = Project.createWithPeriod(
+                color
+                , "   나의 프로젝트    "
+                , Status.SCHEDULE
+                ,period.startDate()
+                ,period.endDate()
+                ,period.actualEndDate()
+                ,"   잘해보자구~"
+                , true
+                , Visibility.PUBLIC);
+
+        //then
+        assertThat(project.getName()).isEqualTo("나의 프로젝트");
+        assertThat(project.getStatus()).isEqualTo(Status.SCHEDULE);
+        assertThat(project.getPeriod().startDate()).isEqualTo(startDate);
+        assertThat(project.getPeriod().endDate()).isEqualTo(endDate);
+        assertThat(project.getPeriod().actualEndDate()).isNull();
         assertThat(project.getDescription()).isEqualTo("잘해보자구~");
         assertThat(project.getIsPublic()).isTrue();
         assertThat(project.getVisibility()).isEqualTo(Visibility.PUBLIC);
