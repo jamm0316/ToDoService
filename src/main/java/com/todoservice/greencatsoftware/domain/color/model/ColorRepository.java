5dee2807d5449d5ed3ea7e21673fab83b3d31738
jamm0316 <jamm0316@naver.com>
2025-08-19 09:22:59 +0900

    refactor: ColorRepository 인터페이스 변경 및 패키지 이동
    1. ColorRepository 인터페이스를 도메인 포트 패키지로 이동한다.
    2. Spring Data JpaRepository 의존성을 제거한다.
    3. CRUD 메서드를 수동으로 정의한다.
    
    Resolves #34

--- src/main/java/com/todoservice/greencatsoftware/domain/color/model/ColorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.todoservice.greencatsoftware.domain.color.model;
-
-import com.todoservice.greencatsoftware.domain.color.entity.Color;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ColorRepository extends JpaRepository<Color, Long> {
-}
