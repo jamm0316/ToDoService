a58ce70f51d9761588ff533cb0e9dbb2f0fc7c6d
jamm0316 <jamm0316@naver.com>
2025-08-19 13:58:44 +0900

    test: getColorByIdOrThrow 테스트 추가
    1. 존재하는 ID의 color 반환 테스트 추가한다.
    2. 존재하지 않는 ID에 대해 BaseException 반환 테스트 추가한다.
    
    Resolves #35

--- src/test/java/com/todoservice/greencatsoftware/domain/color/ColorServiceTest.java
+++ src/test/java/com/todoservice/greencatsoftware/domain/color/ColorServiceTest.java
@@ -1,19 +1,25 @@
 package com.todoservice.greencatsoftware.domain.color;
 
-import com.todoservice.greencatsoftware.domain.color.application.ColorService;
+import com.todoservice.greencatsoftware.common.baseResponse.BaseResponseStatus;
+import com.todoservice.greencatsoftware.common.exception.BaseException;
 import com.todoservice.greencatsoftware.domain.color.entity.Color;
 import com.todoservice.greencatsoftware.domain.color.port.ColorRepository;
+import com.todoservice.greencatsoftware.domain.color.application.ColorService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class ColorServiceTest {
@@ -39,5 +45,24 @@ public class ColorServiceTest {
         assertThat(colors).hasSize(2);
         assertThat(colors.get(0).getName()).isEqualTo("RED");
         assertThat(colors.get(1).getName()).isEqualTo("GREEN");
+    }
+
+    @Test
+    @DisplayName("getColorByIdOrThrow: 존재 시 반환, 없으면 BaseException 반환")
+    public void getByIdOrThrow() throws Exception {
+        //given
+        Optional<Color> red = Optional.of(Color.create("RED", "#FF0000"));
+
+        //when
+        when(colorRepository.findById(1L)).thenReturn(red);
+        when(colorRepository.findById(9L)).thenReturn(Optional.empty());
+
+        //then
+        assertThat(colorService.getColorByIdOrThrow(1L).getId()).isEqualTo(red.get().getId());
+        assertThat(colorService.getColorByIdOrThrow(1L).getName()).isEqualTo(red.get().getName());
+        assertThat(colorService.getColorByIdOrThrow(1L).getHexCode()).isEqualTo(red.get().getHexCode());
+        assertThatThrownBy(() -> colorService.getColorByIdOrThrow(9L))
+                .isInstanceOf(BaseException.class)
+                .hasMessage(BaseResponseStatus.VALIDATION_ERROR.getMessage());
     }
 }
