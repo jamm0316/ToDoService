dddc00116197bc003321d334ae15d61d0f6e88cb
jamm0316 <jamm0316@naver.com>
2025-08-20 13:22:38 +0900

    refactor: Period 객체 전달 방식 변경
    1. createWithPeriod 메서드의 Period 객체 생성 로직 수정한다.
    2. Period 객체를 메서드 호출 시 외부에서 전달받도록 변경한다.
    
    Resolves #44

--- src/main/java/com/todoservice/greencatsoftware/domain/project/domain/entity/Project.java
+++ src/main/java/com/todoservice/greencatsoftware/domain/project/domain/entity/Project.java
@@ -104,11 +104,8 @@ public class Project extends SuperEntity {
         return new Project(color, name, status, null, description, isPublic, visibility);
     }
 
-    //todo: period생성 방식 변경하기
     public static Project createWithPeriod(Color color, String name, Status status,
-                                           LocalDate startDate, LocalDate endDate, LocalDate actualEndDate,
-                                           String description, Boolean isPublic, Visibility visibility) {
-        Period period = Period.of(startDate, endDate, actualEndDate);
+                                           Period period, String description, Boolean isPublic, Visibility visibility) {
         return new Project(color, name, status, period, description, isPublic, visibility);
     }
 
