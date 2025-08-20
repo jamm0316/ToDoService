b20b286e7207dad832f342498d4a38203564dde1
jamm0316 <jamm0316@naver.com>
2025-08-20 12:45:39 +0900

    test: TaskServiceTest 테스트 케이스 추가 및 수정
    1. getTaskByIdOrThrow 테스트 케이스 추가한다.
    2. TaskCreateRequest, Task 객체 mock 사용하여 검증한다.
    3. factory 및 repository 상호작용 여부 확인한다.
    
    Resolves #37

--- src/test/java/com/todoservice/greencatsoftware/domain/task/TaskServiceTest.java
+++ src/test/java/com/todoservice/greencatsoftware/domain/task/TaskServiceTest.java
@@ -1,11 +1,16 @@
 package com.todoservice.greencatsoftware.domain.task;
 
+import com.todoservice.greencatsoftware.common.enums.Status;
+import com.todoservice.greencatsoftware.common.enums.Visibility;
 import com.todoservice.greencatsoftware.domain.color.application.ColorService;
+import com.todoservice.greencatsoftware.domain.color.entity.Color;
 import com.todoservice.greencatsoftware.domain.project.application.ProjectService;
+import com.todoservice.greencatsoftware.domain.project.domain.entity.Project;
 import com.todoservice.greencatsoftware.domain.task.application.TaskFactory;
 import com.todoservice.greencatsoftware.domain.task.application.TaskService;
 import com.todoservice.greencatsoftware.domain.task.domain.entity.Task;
 import com.todoservice.greencatsoftware.domain.task.domain.port.TaskRepository;
+import com.todoservice.greencatsoftware.domain.task.presentation.dto.TaskCreateRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -35,5 +40,23 @@ public class TaskServiceTest {
         //then
         assertThat(taskService.listTask()).hasSize(2);
         verify(taskRepository).findAll();
+    }
+
+    @Test
+    @DisplayName("getTaskByIdOrThrow: 존재, 반환 / 미존재, BaseException 반환")
+    public void getTaskByIdOrThrow() throws Exception {
+        //given
+        TaskCreateRequest request = mock(TaskCreateRequest.class);
+        Task task = mock(Task.class);
+
+        //when
+        when(factory.createTask(request)).thenReturn(task);
+        when(taskRepository.save(task)).thenReturn(task);
+        Task saved = taskService.createTask(request);
+
+        //then
+        assertThat(saved).isSameAs(task);
+        verify(factory).createTask(request);
+        verify(taskRepository).save(task);
     }
 }
