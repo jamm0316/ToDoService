a2169785ae48df6ef72e396aca105d0406049bf5
jamm0316 <jamm0316@naver.com>
2025-08-22 15:20:13 +0900

    refactor: TaskFactory 생성 로직 단순화
    1. Task 생성 시 Color 관련 로직 제거한다.
    2. 코드 간소화 및 가독성 개선한다.
    
    Resolves #84

--- backend/src/main/java/com/todoservice/greencatsoftware/domain/task/application/TaskFactory.java
+++ backend/src/main/java/com/todoservice/greencatsoftware/domain/task/application/TaskFactory.java
@@ -18,15 +18,14 @@ public class TaskFactory {
 
     public Task createTask(TaskCreateRequest request) {
         Project project = projectService.getProjectByIdOrThrow(request.projectId());
-        Color color = colorService.getColorByIdOrThrow(request.colorId());
         Schedule schedule = Schedule.of(
                 request.schedule().startDate(), request.schedule().startTime(), request.schedule().startTimeEnabled(),
                 request.schedule().dueDate(), request.schedule().dueTime(), request.schedule().dueTimeEnabled());
 
         return (schedule.isNull())
-                ? Task.create(project, color, request.priority(), request.title(), request.description(),
+                ? Task.create(project, request.priority(), request.title(), request.description(),
                 request.dayLabel(), request.status())
-                : Task.createWithSchedule(project, color, request.priority(), request.title(), request.description(),
+                : Task.createWithSchedule(project, request.priority(), request.title(), request.description(),
                 request.dayLabel(), schedule, request.status());
     }
 }
